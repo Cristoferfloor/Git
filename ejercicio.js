9fcd921b67c09d432a744939b772ceb797322b1d
ARIEL CRISTOFER MOPOSITA LAMINA <ariel.moposita@epn.edu.ec>
2023-12-07 21:22:40 -0500

    repaso git

---
+++ ejercicio.js
@@ -0,0 +1,11 @@
+saludar = function(){
+
+    let cmpImagensaludo;
+
+    cmpImagensaludo= document.getElementById("imgsaludo");
+
+    cmpImagensaludo.src="./Imagenes/gif2.gif"
+
+    console.log(" Ingresa saluda ")
+
+}
